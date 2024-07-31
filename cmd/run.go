a90dedda80c122364ca3786d140ed01d33411ed4
tenax66 <hiyoshir+tenax66@gmail.com>
2024-07-31 22:56:55 +0900

    create run command

--- cmd/run.go
+++ cmd/run.go
@@ -8,15 +8,19 @@ import (
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run benchmarks.",
-	Long:  `Runs provided number of requests.`,
+	Use:   "run [OPTIONS] <URL>",
+	Short: "Run benchmarks for <URL>.",
+	Long:  `Runs provided number of requests to <URL>.`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		url := args[0]
+		n, _ := cmd.Flags().GetInt("number")
+		fmt.Printf("%v times %v\n", url, n)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+	runCmd.DisableFlagsInUseLine = true
 	runCmd.Flags().IntP("number", "n", 1, "number of requests to run")
 }
